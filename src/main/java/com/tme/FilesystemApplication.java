5b305aa7d59cded47c980adb9a62d67d9fda2313
michaellion <ljnwsbs@gmail.com>
2018-07-27 16:40:09 +0800

    filesystem submit

---
+++ src/main/java/com/tme/FilesystemApplication.java
@@ -0,0 +1,11 @@
+package com.tme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FilesystemApplication.class, args);
+    }
+}
